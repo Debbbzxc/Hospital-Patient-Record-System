a369f16e14a5e61d6dfac45fb11cb16f631466d3
Mark Daves Baltazar <135539158+Debbbzxc@users.noreply.github.com>
2025-03-31 03:02:38 +0800

    Add project files.

---
+++ BALTAZAR - EDP Activity 13/Form1.Designer.vb
@@ -0,0 +1,289 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtRoom = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDiagnosis = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.dgvPatients = New System.Windows.Forms.DataGridView()
+        Me.btnShowRecords = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvPatients, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnDelete)
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.btnSave)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtID)
+        Me.GroupBox1.Controls.Add(Me.txtRoom)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtDiagnosis)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtAge)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Location = New System.Drawing.Point(228, 114)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(321, 244)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Patient Information"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(26, 202)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(269, 26)
+        Me.btnDelete.TabIndex = 12
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(23, 33)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(18, 13)
+        Me.Label10.TabIndex = 3
+        Me.Label10.Text = "ID"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(26, 170)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(269, 26)
+        Me.btnSave.TabIndex = 11
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 137)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(55, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Room No."
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(88, 30)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(102, 20)
+        Me.txtID.TabIndex = 0
+        '
+        'txtRoom
+        '
+        Me.txtRoom.Location = New System.Drawing.Point(88, 134)
+        Me.txtRoom.Name = "txtRoom"
+        Me.txtRoom.Size = New System.Drawing.Size(102, 20)
+        Me.txtRoom.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Diagnosis"
+        '
+        'txtDiagnosis
+        '
+        Me.txtDiagnosis.Location = New System.Drawing.Point(88, 108)
+        Me.txtDiagnosis.Name = "txtDiagnosis"
+        Me.txtDiagnosis.Size = New System.Drawing.Size(207, 20)
+        Me.txtDiagnosis.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Age"
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(88, 82)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(102, 20)
+        Me.txtAge.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(88, 56)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(207, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnNew
+        '
+        Me.btnNew.Location = New System.Drawing.Point(228, 71)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(158, 37)
+        Me.btnNew.TabIndex = 9
+        Me.btnNew.Text = "New Record"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(391, 71)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(158, 37)
+        Me.btnUpdate.TabIndex = 10
+        Me.btnUpdate.Text = "Update/Delete Record"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 15)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(198, 234)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'dgvPatients
+        '
+        Me.dgvPatients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvPatients.Location = New System.Drawing.Point(12, 364)
+        Me.dgvPatients.Name = "dgvPatients"
+        Me.dgvPatients.Size = New System.Drawing.Size(537, 212)
+        Me.dgvPatients.TabIndex = 12
+        '
+        'btnShowRecords
+        '
+        Me.btnShowRecords.Location = New System.Drawing.Point(12, 332)
+        Me.btnShowRecords.Name = "btnShowRecords"
+        Me.btnShowRecords.Size = New System.Drawing.Size(210, 26)
+        Me.btnShowRecords.TabIndex = 13
+        Me.btnShowRecords.Text = "Show Records"
+        Me.btnShowRecords.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.PictureBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 65)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(210, 261)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btnConnect)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(537, 47)
+        Me.GroupBox3.TabIndex = 15
+        Me.GroupBox3.TabStop = False
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(6, 12)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(525, 27)
+        Me.btnConnect.TabIndex = 16
+        Me.btnConnect.Text = "Connect to Baltazar Medical Center Database"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(560, 585)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnShowRecords)
+        Me.Controls.Add(Me.dgvPatients)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnNew)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Baltazar Medical Center - Patient Record"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvPatients, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtRoom As TextBox
+    Friend WithEvents txtDiagnosis As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents btnNew As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents dgvPatients As DataGridView
+    Friend WithEvents btnShowRecords As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnConnect As Button
+End Class
---
+++ BALTAZAR - EDP Activity 13/Form1.vb
@@ -0,0 +1,184 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Dim buttonClicked As String = ""
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=localhost; userid=root; password=root; database=patients_db;"
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to the database was successful!")
+            btnNew.Enabled = True
+            btnUpdate.Enabled = True
+            btnShowRecords.Enabled = True
+            btnConnect.Enabled = False
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        buttonClicked = "New Record"
+        btnSave.Text = "Add"
+        btnNew.Enabled = False
+        btnUpdate.Enabled = True
+        txtID.Enabled = False
+        txtName.Enabled = True
+        txtAge.Enabled = True
+        txtDiagnosis.Enabled = True
+        txtRoom.Enabled = True
+        btnSave.Enabled = True
+        btnDelete.Enabled = False
+        txtID.Text = ""
+        txtName.Text = ""
+        txtAge.Text = ""
+        txtDiagnosis.Text = ""
+        txtRoom.Text = ""
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        buttonClicked = "Update Record"
+        btnSave.Text = "Update"
+        btnUpdate.Enabled = False
+        btnNew.Enabled = True
+        txtID.Enabled = True
+        txtName.Enabled = True
+        txtAge.Enabled = True
+        txtDiagnosis.Enabled = True
+        txtRoom.Enabled = True
+        btnSave.Enabled = True
+        btnDelete.Enabled = True
+        txtID.Text = ""
+        txtName.Text = ""
+        txtAge.Text = ""
+        txtDiagnosis.Text = ""
+        txtRoom.Text = ""
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If buttonClicked = "New Record" Then
+            Dim query As String = "INSERT INTO patients_tbl (name, age, diagnosis, room) VALUES (@name, @age, @diagnosis, @room)"
+            Try
+                If txtName.Text = "" Or txtAge.Text = "" Or txtDiagnosis.Text = "" Or txtRoom.Text = "" Then
+                    Throw (New Exception("Input field/s cannot be empty!"))
+                End If
+                Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=patients_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@name", txtName.Text)
+                        cmd.Parameters.AddWithValue("@age", Convert.ToInt32(txtAge.Text))
+                        cmd.Parameters.AddWithValue("@diagnosis", txtDiagnosis.Text)
+                        cmd.Parameters.AddWithValue("@room", Convert.ToInt32(txtRoom.Text))
+                        cmd.ExecuteNonQuery()
+                        MessageBox.Show("New patient has been successfully added!")
+                        btnNew.Enabled = True
+                        btnUpdate.Enabled = True
+                        txtID.Enabled = False
+                        txtName.Enabled = False
+                        txtAge.Enabled = False
+                        txtDiagnosis.Enabled = False
+                        txtRoom.Enabled = False
+                        btnSave.Enabled = False
+                        btnDelete.Enabled = False
+                    End Using
+                End Using
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+        ElseIf buttonClicked = "Update Record" Then
+            Dim query As String = "UPDATE patients_tbl SET name=@name, age=@age, diagnosis=@diagnosis, room=@room WHERE id=@id"
+            Try
+                If txtName.Text = "" Or txtAge.Text = "" Or txtDiagnosis.Text = "" Or txtRoom.Text = "" Or txtID.Text = "" Then
+                    Throw (New Exception("Input field/s cannot be empty!"))
+                End If
+                Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=patients_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@name", txtName.Text)
+                        cmd.Parameters.AddWithValue("@age", Convert.ToInt32(txtAge.Text))
+                        cmd.Parameters.AddWithValue("@diagnosis", txtDiagnosis.Text)
+                        cmd.Parameters.AddWithValue("@room", Convert.ToInt32(txtRoom.Text))
+                        cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtID.Text))
+                        cmd.ExecuteNonQuery()
+                        MessageBox.Show("Patient's record has been successfully updated!")
+                        btnNew.Enabled = True
+                        btnUpdate.Enabled = True
+                        txtID.Enabled = False
+                        txtName.Enabled = False
+                        txtAge.Enabled = False
+                        txtDiagnosis.Enabled = False
+                        txtRoom.Enabled = False
+                        btnSave.Enabled = False
+                        btnDelete.Enabled = False
+                    End Using
+                End Using
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+        Else
+            MessageBox.Show("Choose an option first!")
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If buttonClicked = "Update Record" Then
+            Dim query As String = "DELETE FROM patients_tbl WHERE id=@id"
+            Try
+                If txtID.Text = "" Then
+                    Throw (New Exception("ID input field cannot be empty!"))
+                End If
+                Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=patients_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("id", Convert.ToInt32(txtID.Text))
+                        cmd.ExecuteNonQuery()
+                        MessageBox.Show("Patient's record has been successfully deleted!")
+                        btnNew.Enabled = True
+                        btnUpdate.Enabled = True
+                        txtID.Enabled = False
+                        txtName.Enabled = False
+                        txtAge.Enabled = False
+                        txtDiagnosis.Enabled = False
+                        txtRoom.Enabled = False
+                        btnSave.Enabled = False
+                        btnDelete.Enabled = False
+                    End Using
+                End Using
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnShowRecords_Click(sender As Object, e As EventArgs) Handles btnShowRecords.Click
+        Dim query As String = "SELECT id, name, age, diagnosis, room FROM `patients_db`.`patients_tbl`"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=patients_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                dgvPatients.DataSource = table
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnNew.Enabled = False
+        btnUpdate.Enabled = False
+        btnSave.Enabled = False
+        btnDelete.Enabled = False
+        btnShowRecords.Enabled = False
+        txtID.Enabled = False
+        txtName.Enabled = False
+        txtAge.Enabled = False
+        txtDiagnosis.Enabled = False
+        txtRoom.Enabled = False
+    End Sub
+End Class
